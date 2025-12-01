539c7a38c24f1083e85f07d275e7a584fce40d0d
aGith0bszkola <githubszkola@gmail.com>
2025-12-01 15:21:15 +0100

    dziajcy program + tlumaczenia

--- app/src/main/java/pl/zabrze/zs10/flaga/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/flaga/MainActivity.java
@@ -4,21 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int licznik = 0;
+    TextView textViewPolecenie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewPolecenie = findViewById(R.id.textVievPolecenie);
     }
 
     public void dobry(View view) {
-        Toast.makeText(this, "tego przycisku nie wybieraj", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, R.string.ostrzezenie, Toast.LENGTH_SHORT).show();
     }
 
     public void ukryj(View view) {
         view.setVisibility(View.INVISIBLE);
+        licznik++;
+        if (licznik == 4){
+            Toast.makeText(this, "To jest flaga polski", Toast.LENGTH_SHORT).show();
+            textViewPolecenie.setText(R.string.podsumowanie);
+        }
     }
 }
