08cdce2413f502dcc1927ae263c88f08c5c74f38
aGith0bszkola <githubszkola@gmail.com>
2025-12-01 15:11:02 +0100

    dziajcy program

--- app/src/main/java/pl/zabrze/zs10/flaga/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/flaga/MainActivity.java
@@ -3,6 +3,8 @@ package pl.zabrze.zs10.flaga;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dobry(View view) {
+        Toast.makeText(this, "tego przycisku nie wybieraj", Toast.LENGTH_SHORT).show();
+    }
+
+    public void ukryj(View view) {
+        view.setVisibility(View.INVISIBLE);
     }
 }
